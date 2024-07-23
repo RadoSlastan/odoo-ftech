07e4c9c6a0ad4d9b9b607cedb769046200755837
Yasmine Hamouda <hamouda.yasmine9@gmail.com>
2024-07-23 17:51:24 +0200

    added search by the properties

--- f_tech/__manifest__.py
+++ f_tech/__manifest__.py
@@ -10,13 +10,14 @@
     'category': '',
     'version': '17.0.1.0.0',
     'sequence': 1,
-    'depends': ['base', 'product', 'sale','purchase'],
+    'depends': ['base', 'product', 'sale', 'purchase'],
     'data': [
         'security/ir.model.access.csv',
         'views/product_template_form_view.xml',
         'views/sale_order_view.xml',
         'views/product_view_kanban_catalog.xml',
         'views/product_view.xml',
+        'views/product_template_search_view.xml',
         'wizard/view_sale_order_line_to_purchase_wizard_form.xml',
 
     ],
