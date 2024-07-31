39d1ea7c6454837b7802580c22740239f0620b81
Yasmine Hamouda <hamouda.yasmine9@gmail.com>
2024-07-31 22:46:49 +0200

    fixes in add and remove button in so catalog

--- f_tech/models/product_product.py
+++ f_tech/models/product_product.py
@@ -1,47 +1,72 @@
 from odoo import models, fields, api
 from odoo.tools.misc import unique
+
+
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
+    order_id = fields.Many2one('sale.order', string='Sale Order', compute='_compute_order_id', store=False)
+    product_so_qty = fields.Integer('Quantity', compute='_compute_product_so_qty', store=False)
+    @api.depends('order_id')
+    def _compute_product_so_qty(self):
+        for product in self:
+            order_lines = product.order_id.order_line.filtered(lambda line: line.product_id.id == product.id)
+            product.product_so_qty = sum(line.product_uom_qty for line in order_lines)
+            if order_lines:
+                product.product_so_qty = sum(line.product_uom_qty for line in order_lines)
+            else:
+                product.product_so_qty = 0
+    @api.depends()
+    def _compute_order_id(self):
+        for product in self:
+            order_id = dict(product.env.context or {}).get('order_id')
+            product.order_id = self.env['sale.order'].browse(order_id)
 
+    is_in_sale_order = fields.Boolean(
+        string='In Sale Order',
+        compute='_compute_is_in_sale_order',
+        default=True
+    )
+
+    @api.depends('order_id')
+    def _compute_is_in_sale_order(self):
+        for product in self:
+            product.is_in_sale_order = any(line.product_id == product for line in product.order_id.order_line)
 
     def action_add_to_sale_order(self):
-        order_id = dict(self.env.context or {}).get('order_id')
-        sale_order = self.env['sale.order'].browse(order_id)
-        if not sale_order:
+        if not self.order_id:
             return
         product_ids = self.env.context.get('active_ids', [])
         print(product_ids)
         for product_id in product_ids:
             product = self.env['product.product'].browse(product_id)
-            sale_order.order_line.create({
-                'order_id': sale_order.id,
+            self.order_id.order_line.create({
+                'order_id': self.order_id.id,
                 'product_id': product.id,
                 'name': product.name,
-                'product_uom_qty': 1,  # default to 1 quantity, you can customize this
+                'product_uom_qty': 1,
                 'price_unit': product.lst_price,
             })
         return True
 
     def action_remove_from_sale_order(self):
-        order_id = dict(self.env.context or {}).get('order_id')
         product_ids = self.env.context.get('active_ids', [])
-        sale_order = self.env['sale.order'].browse(order_id)
-
         for product_id in product_ids:
-            order_line = sale_order.order_line.filtered(lambda line: line.product_id.id == product_id)
+            order_line = self.order_id.order_line.filtered(lambda line: line.product_id.id == product_id)
             if order_line:
                 order_line.unlink()
         return True
 
     def action_add_qty(self):
         pass
+
     def action_sub_qty(self):
         pass
+
     @api.depends('name', 'default_code', 'product_tmpl_id')
     @api.depends_context('display_default_code', 'seller_id', 'company_id', 'partner_id')
     def _compute_display_name(self):
-   # overrided this def to make the discription in the SO without the internal reference
+        # overrided this def to make the discription in the SO without the internal reference
         def get_display_name(name, code):
 
             return name
