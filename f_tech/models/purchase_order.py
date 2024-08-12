809610bce505916ccd0a9c415dfd0a17d7663287
Yasmine Hamouda <hamouda.yasmine9@gmail.com>
2024-08-12 21:48:30 +0200

    fixed PO tree catalog

--- f_tech/models/purchase_order.py
+++ f_tech/models/purchase_order.py
@@ -5,3 +5,10 @@ class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
     attachment_ids = fields.Many2many('ir.attachment', string="Attachments")
+
+    def action_add_from_catalog(self):
+        #Add the product's tree view .
+        action = super().action_add_from_catalog()
+        tree_view_id = self.env.ref('f_tech.product_view_tree_catalog').id
+        action['views'][0] = (tree_view_id, 'tree')
+        return action
